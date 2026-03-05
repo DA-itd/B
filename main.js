fc4dd7060501482ff98b2970dbdf17524113be93
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2026-03-04 20:03:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -450,7 +450,7 @@ const Dashboard = ({ user, onLogout }) => {
 
                             {/* Columna 3: Botones Acción */}
                             <div className="col-span-1 md:col-span-3 flex justify-start md:justify-end gap-2">
-                                {item.link && item.link !== '#' ? (
+                                {item.link && item.link !== '#' && item.status === 'ENVIADO' ? (
                                     <>
                                         <button 
                                             onClick={() => handleShareEmail(item)}
@@ -472,7 +472,7 @@ const Dashboard = ({ user, onLogout }) => {
                                     </>
                                 ) : (
                                     <span className="text-xs text-gray-400 italic px-4 py-2 bg-gray-50 rounded border border-gray-100 w-full md:w-auto text-center">
-                                        No disponible
+                                        {item.status !== 'ENVIADO' ? 'No Aprobado, Revisar con su instructor' : 'No disponible'}
                                     </span>
                                 )}
                             </div>
